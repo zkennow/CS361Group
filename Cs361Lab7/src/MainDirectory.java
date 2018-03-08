b1c9e0c0842f73d115ce8bec393d050c21dc4706
Nic with a C <Nic with a C@NicPC>
2018-03-08 17:05:52 -0600

    Eclipse not showing me potential errors so... I think this is right

--- Cs361Lab7/src/MainDirectory.java
+++ Cs361Lab7/src/MainDirectory.java
@@ -1,4 +1,31 @@
+import java.util.ArrayList;
+import java.util.Collection;
+
+import com.google.gson.Gson;
+
+import test.Employee;
+import test.Participants;
+
 
 public class MainDirectory {
 
+	ArrayList<Employee> employees = new ArrayList<Employee>();
+	Gson gson = new Gson();
+	
+	public void add(String json) {
+		employees = gson.fromJson(json, new TypeToken<ArrayList<Employee>>(){}.getType());
+	}
+	
+	public void print() {
+		int numEmployees = employees.size();
+		for (int i = 0; i < numEmployees; i++) {
+			System.out.println(employees.getIndex(i).toString());
+		}
+		System.out.println("");
+	}
+	
+	public void clear() {
+		employees.clear();
+	}
+	
 }
